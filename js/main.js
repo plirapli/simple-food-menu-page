baffea6b3e67d315a73561d05cc2c0e0f5430e03
rafliyulistiawan <rafliyulistiawan@gmail.com>
2020-12-09 23:12:24 +0700

    responsiveness oke

--- js/main.js
+++ js/main.js
@@ -2,29 +2,32 @@ let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
 
-       let menu = JSON.parse(xhttp.responseText).menu;
+        let menu = JSON.parse(xhttp.responseText).menu;
+        let mainMenu = menu.slice(0, 8);
 
-       let output = '';
-       for (let i = 0; i < menu.length; i++) {
+        document.getElementById('loadMore').addEventListener('click', () => {mainMenu = menu})
+
+        let output = '';
+        mainMenu.forEach(m => {
            output += 
-            `<div class="menu menu-${menu[i].id}">
+            `<div class="menu menu-${m.id}">
                 <div class="menu-img">
-                    <img src="${menu[i].img}" alt="">
+                    <img src="${m.img}" alt="">
                 </div>
                 <p class="menu-name">
-                    ${menu[i].name}
+                    ${m.name}
                 </p>
-                <p class="menu-price">Rp${menu[i].price}</p>
+                <p class="menu-price">Rp${m.price}</p>
                 <div class="add-chart"><i class="fas fa-plus"></i></div>
             </div>`
-       }
+       });
        document.getElementById('menu-content').innerHTML = output;
-
     }
 };
 xhttp.open("GET", "../json/menu.json", true);
 xhttp.send();
 
+// Navigation Menu
 document.querySelector('.menu-bar').addEventListener('click', () => {
     document.querySelector('.nav').classList.toggle('show')
     document.querySelector('.menu-bar').classList.toggle('close')
