c8d38201e7e9ea3d28465ad75d33c2360a5c28ab
rafliyulistiawan <rafliyulistiawan@gmail.com>
2020-12-10 20:54:29 +0700

    add intersectionObserver on menu

--- js/main.js
+++ js/main.js
@@ -3,25 +3,42 @@ xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
 
         let menu = JSON.parse(xhttp.responseText).menu;
-        let mainMenu = menu.slice(0, 8);
+        // let mainMenu = menu.slice(0, 8);
 
-        document.getElementById('loadMore').addEventListener('click', () => {mainMenu = menu})
+        // document.getElementById('loadMore').addEventListener('click', () => {mainMenu = menu})
 
         let output = '';
-        mainMenu.forEach(m => {
-           output += 
+        menu.forEach(m => {
+            output += 
             `<div class="menu menu-${m.id}">
                 <div class="menu-img">
                     <img src="${m.img}" alt="">
                 </div>
-                <p class="menu-name">
-                    ${m.name}
-                </p>
+                <p class="menu-name">${m.name}</p>
                 <p class="menu-price">Rp${m.price}</p>
                 <div class="add-chart"><i class="fas fa-plus"></i></div>
             </div>`
-       });
-       document.getElementById('menu-content').innerHTML = output;
+        });
+        document.getElementById('menu-content').innerHTML = output;
+
+        const animation = document.querySelectorAll('.menu');
+        const options = {
+            rootMargin: "-100px"
+        }
+
+        observer = new IntersectionObserver((entries, observer) => {
+            // console.log(entries);
+            entries.forEach(entry => {
+                if(entry.intersectionRatio > 0) {
+                    entry.target.style.animation = `anim1 0.8s forwards ease`;
+                    observer.unobserve(entry.target)
+                } else {
+                    entry.target.style.animation = 'none'
+                }
+            })
+        }, options)
+
+        animation.forEach(anim => observer.observe(anim))
     }
 };
 xhttp.open("GET", "../json/menu.json", true);
