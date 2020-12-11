fdcf434dcf723c06faf96b57b1116e556dada9b8
rafliyulistiawan <rafliyulistiawan@gmail.com>
2020-12-12 00:58:50 +0700

    tab oke

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-let xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        let menu = JSON.parse(xhttp.responseText).menu;
-        
+document.getElementById('pizza').addEventListener('click', getPizza)
+document.getElementById('all').addEventListener('click', getAll)
+// document.getElementById('pizza').addEventListener('click', getPizza)
+
+function getAll() {
+    const navName = document.querySelectorAll('.nav-name');
+    const activeNav = document.querySelector('.nav-name.active')
+    const pizza = document.getElementById('all');
+    
+    navName.forEach(nav => {
+        // if(nav )
+        if (nav === activeNav) {
+            nav.classList.remove('active')
+        } else {all.classList.add('active')}
+    })
+
+    fetch('../json/menu.json')
+    .then(res => res.json())
+    .then(data => {
         const menuContent = document.getElementById('menu-content');
         const search = document.getElementById('search');
+        let menu = data.menu;
 
         // Load Menu
         let output = '';
@@ -21,48 +36,86 @@ xhttp.onreadystatechange = function() {
         });
         menuContent.innerHTML = output;
 
-        // Animate On Scroll
-        const animation = document.querySelectorAll('.menu');
-        const options = {
-            rootMargin: "-100px"
-        }
+        aos();
+    })
+}
 
-        observer = new IntersectionObserver((entries, observer) => {
-            // console.log(entries);
-            entries.forEach(entry => {
-                if(entry.intersectionRatio > 0) {
-                    entry.target.style.animation = `anim1 0.8s forwards ease`;
-                    observer.unobserve(entry.target)
-                } else {
-                    entry.target.style.animation = 'none'
-                }
-            })
-        }, options)
+function getPizza() {
+    const navName = document.querySelectorAll('.nav-name');
+    const activeNav = document.querySelector('.nav-name.active')
+    const pizza = document.getElementById('pizza');
+    
+    navName.forEach(nav => {
+        // if(nav )
+        if (nav === activeNav) {
+            nav.classList.remove('active')
+        } else {pizza.classList.add('active')}
+    })
 
-        animation.forEach(anim => observer.observe(anim))
-
-        // Search Filter
-        search.addEventListener('input', () => {
-            let value = document.getElementById('search').value.toUpperCase()
-            console.log(value)
-            let menu = document.querySelectorAll('.menu')
-
-            for (let i = 0; i < menu.length; i++) {
-                let name = menu[i].getElementsByClassName('menu-name')[0]
-
-                if (name.innerHTML.toUpperCase().indexOf(value) > -1) {
-                    menu[i].style.display = ''
-                } else {
-                    menu[i].style.display = 'none'
-                }
-            }
-
+    fetch('../json/menu.json')
+        .then(res => res.json())
+        .then(data => {
+            let menus = data.menu
+            const pizza = menus.filter(menu => menu.type === 'pizza')
+            
+        // Load Menu
+        const menuContent = document.getElementById('menu-content');
+        let output = '';
+        pizza.forEach(m => {
+            output += 
+            `<div class="menu menu-${m.id}">
+                <div class="menu-img">
+                    <img src="${m.img}" alt="">
+                </div>
+                <p class="menu-name">${m.name}</p>
+                <p class="menu-price">Rp${m.price}</p>
+                <div class="add-chart"><i class="fas fa-plus"></i></div>
+            </div>`
         });
+        menuContent.innerHTML = output;
 
-    }
-};
-xhttp.open("GET", "../json/menu.json", true);
-xhttp.send();
+        aos();
+        })
+}
+
+getAll();
+
+// Animate On Scroll
+function aos() {
+    const animation = document.querySelectorAll('.menu');
+    const options = { rootMargin: "-28px" }
+
+    observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.intersectionRatio > 0) {
+                entry.target.style.animation = `anim1 0.8s forwards ease`;
+                observer.unobserve(entry.target)
+            } else {
+                entry.target.style.animation = 'none'
+            }
+        })
+    }, options)
+
+    animation.forEach(anim => observer.observe(anim))
+}
+
+//         // Search Filter
+//         search.addEventListener('input', () => {
+//             let value = document.getElementById('search').value.toUpperCase()
+//             console.log(value)
+//             let menu = document.querySelectorAll('.menu')
+
+//             for (let i = 0; i < menu.length; i++) {
+//                 let name = menu[i].getElementsByClassName('menu-name')[0]
+
+//                 if (name.innerHTML.toUpperCase().indexOf(value) > -1) {
+//                     menu[i].style.display = ''
+//                 } else {
+//                     menu[i].style.display = 'none'
+//                 }
+//             }
+
+//         });
 
 // Navigation Menu
 document.querySelector('.menu-bar').addEventListener('click', () => {
